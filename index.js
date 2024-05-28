63760ff17b48560dd8e65f2633a34f4e7cbbbed0
eliangerard <eliangerardgg@gmail.com>
2024-05-28 14:24:00 -0600

    Update routes for API endpoints

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const connection = require("./config/database");
-const events = require("./src/routes/events");
-const auth = require("./src/routes/auth");
+const api = require("./src/routes/api")
 
 const app = express();
 
@@ -12,9 +11,11 @@ app.use(express.json({
     limit: "50mb"
 }));
 app.use(cors());
+app.use('/admin', express.static("public/admin"));
+app.use('/', express.static("public/client"));
+app.use(express.static("public"));
 
-app.use('/events', events);
-app.use('/auth', auth);
+app.use("/api", api);
 
 connection.on("error", console.error.bind(console, "connection error:"));
 connection.once("open", () => {
