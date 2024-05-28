65a1d89391063c810e536cb3669662e4535fa942
eliangerard <eliangerardgg@gmail.com>
2024-05-28 14:24:22 -0600

    Update verifySession middleware to handle invalid tokens

--- src/middlewares/verifySession.js
+++ src/middlewares/verifySession.js
@@ -15,19 +15,23 @@ function getKey(header, callback) {
 }
 
 const verifySession = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-    console.log("Token", token);
-    jwt.verify(token, getKey, { algorithms: ['RS256'] }, (err, decoded) => {
-
-
-        console.log(err, decoded);
-        if (err) {
-            res.status(401).json({ message: 'Token inválido' });
-        } else {
-            req.decoded = decoded;
-            next();
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if (!token) {
+            res.status(401).json({ message: 'Token not found' });
         }
-    });
+        jwt.verify(token, getKey, { algorithms: ['RS256'] }, (err, decoded) => {
+            if (err) {
+                res.status(401).json({ message: 'Token inválido' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    }
+    catch (error) {
+        res.status(401).json({ message: 'Token inválido' });
+    }
 }
 
 module.exports = verifySession;
