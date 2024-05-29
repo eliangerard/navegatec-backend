0ed947e0c65461f532cf8cd589b6904bceea094d
eliangerard <eliangerardgg@gmail.com>
2024-05-28 20:19:08 -0600

    Add endpoint to allow users to be added in the whitelist

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getToken, refresh, verify } = require('../controllers/authController');
+const { getToken, refresh, verify, addAllowedUser } = require('../controllers/authController');
 const verifySession = require('../middlewares/verifySession');
 const auth = express.Router();
 
 auth.get('/token', getToken);
 auth.get('/refresh', refresh);
 auth.get('/verify', verifySession, verify);
+auth.post('/allow', verifySession, addAllowedUser);
 
 module.exports = auth;
