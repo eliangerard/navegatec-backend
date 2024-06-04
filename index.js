9a681b84e9e53903a624b8c423dc876328e4f977
eliangerard <eliangerardgg@gmail.com>
2024-06-03 19:57:29 -0600

    Update static file serving paths for admin and client pages

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ app.use(express.json({
     limit: "50mb"
 }));
 app.use(cors());
-app.use('/admin', express.static("public/admin"));
-app.use('/', express.static("public/client"));
+app.use("/api/*", api);
+
+app.use('/admin/*', express.static("public/admin"));
+app.use('/*', express.static("public/client"));
 app.use(express.static("public"));
 
-app.use("/api", api);
 
 connection.on("error", console.error.bind(console, "connection error:"));
 connection.once("open", () => {
